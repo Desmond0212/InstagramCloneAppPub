38ff30624cccc883fc7bb63e5a95d869aca0c975
Desmond Wong <desmond.9602@gmail.com>
2020-04-25 18:37:27 +0800

    + Added BottomNavigationView. + Changed Icon Set.

--- app/src/main/java/com/example/instagramcloneapp/MainActivity.kt
+++ app/src/main/java/com/example/instagramcloneapp/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.instagramcloneapp
 
 import android.os.Bundle
+import android.widget.TextView
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -8,19 +9,55 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity()
+{
+    private lateinit var textView: TextView
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
-        val navController = findNavController(R.id.nav_host_fragment)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications))
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+        textView = findViewById(R.id.message)
+        navView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
+    }
+
+    private val onNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId)
+        {
+            R.id.nav_home ->
+            {
+                textView.text = "Home"
+                return@OnNavigationItemSelectedListener true
+            }
+
+            R.id.nav_search ->
+            {
+                textView.text = "Search"
+                return@OnNavigationItemSelectedListener true
+            }
+
+            R.id.nav_add_post ->
+            {
+                textView.text = "Add Post"
+                return@OnNavigationItemSelectedListener true
+            }
+
+            R.id.nav_notifications ->
+            {
+                textView.text = "Notification"
+                return@OnNavigationItemSelectedListener true
+            }
+
+            R.id.nav_profile ->
+            {
+                textView.text = "Profile"
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+
+        false
     }
 }
