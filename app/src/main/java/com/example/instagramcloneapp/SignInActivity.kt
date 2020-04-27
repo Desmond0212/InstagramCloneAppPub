6399fd42f0be8edc825ab6f9c477b8639d71d0c7
Desmond Wong <desmond.9602@gmail.com>
2020-04-28 00:26:39 +0800

    + Added Google Service .json + Added Sign In, Sign Out and Sign Up Features with Google Firebase.

--- app/src/main/java/com/example/instagramcloneapp/SignInActivity.kt
+++ app/src/main/java/com/example/instagramcloneapp/SignInActivity.kt
@@ -1,13 +1,16 @@
 package com.example.instagramcloneapp
 
+import android.app.ProgressDialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_sign_in.*
 
 class SignInActivity : AppCompatActivity()
 {
-
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
@@ -15,6 +18,65 @@ class SignInActivity : AppCompatActivity()
 
         signup_link_btn.setOnClickListener {
             startActivity(Intent(this, SignUpActivity::class.java))
+        }
+
+        login_btn.setOnClickListener {
+            loginUser()
+        }
+    }
+
+    override fun onStart()
+    {
+        super.onStart()
+
+        if (FirebaseAuth.getInstance().currentUser != null)
+        {
+            val intent = Intent(this, MainActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
+            startActivity(intent)
+            finish()
+        }
+    }
+
+    private fun loginUser()
+    {
+        val email = email_login.text.toString()
+        val password = password_login.text.toString()
+
+        when
+        {
+            TextUtils.isEmpty(email) -> Toast.makeText(this, "Email is required.", Toast.LENGTH_LONG).show()
+            TextUtils.isEmpty(password) -> Toast.makeText(this, "Password is required.", Toast.LENGTH_LONG).show()
+
+            else ->
+            {
+                val progressDialog = ProgressDialog(this)
+                progressDialog.setTitle("Login")
+                progressDialog.setMessage("Please wait, this may take a while...")
+                progressDialog.setCanceledOnTouchOutside(false)
+                progressDialog.show()
+
+                val mAuth: FirebaseAuth = FirebaseAuth.getInstance()
+
+                mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener { task ->
+                    if (task.isSuccessful)
+                    {
+                        progressDialog.dismiss()
+
+                        val intent = Intent(this, MainActivity::class.java)
+                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
+                        startActivity(intent)
+                        finish()
+                    }
+                    else
+                    {
+                        val message = task.exception!!.toString()
+                        Toast.makeText(this, "Error: $message" , Toast.LENGTH_LONG).show()
+                        FirebaseAuth.getInstance().signOut()
+                        progressDialog.dismiss()
+                    }
+                }
+            }
         }
     }
 }
--- app/src/main/java/com/example/instagramcloneapp/SignUpActivity.kt
+++ app/src/main/java/com/example/instagramcloneapp/SignUpActivity.kt
@@ -1,13 +1,18 @@
 package com.example.instagramcloneapp
 
+import android.app.ProgressDialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 import kotlinx.android.synthetic.main.activity_sign_up.*
 
 class SignUpActivity : AppCompatActivity()
 {
-
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
@@ -17,6 +22,85 @@ class SignUpActivity : AppCompatActivity()
             startActivity(Intent(this, SignInActivity::class.java))
         }
 
-        /*Desmond Stop: Finished EP6, Start Ep7*/
+        signup_btn.setOnClickListener {
+            createAccount()
+        }
+    }
+
+    private fun createAccount()
+    {
+        val fullName = fullname_signup.text.toString()
+        val userName = username_signup.text.toString()
+        val email = email_signup.text.toString()
+        val password = password_signup.text.toString()
+
+        when
+        {
+            TextUtils.isEmpty(fullName) -> Toast.makeText(this, "Full Name is required.", Toast.LENGTH_LONG).show()
+            TextUtils.isEmpty(userName) -> Toast.makeText(this, "User Name is required.", Toast.LENGTH_LONG).show()
+            TextUtils.isEmpty(email) -> Toast.makeText(this, "Email is required.", Toast.LENGTH_LONG).show()
+            TextUtils.isEmpty(password) -> Toast.makeText(this, "Password is required.", Toast.LENGTH_LONG).show()
+
+            else ->
+            {
+                val progressDialog = ProgressDialog(this)
+                progressDialog.setTitle("Sign Up")
+                progressDialog.setMessage("Please wait, this may take a while...")
+                progressDialog.setCanceledOnTouchOutside(false)
+                progressDialog.show()
+
+                val mAuth: FirebaseAuth = FirebaseAuth.getInstance()
+
+                mAuth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener { task ->
+                        if (task.isSuccessful)
+                        {
+                            saveUserInfo(fullName, userName, email, password, progressDialog)
+                        }
+                        else
+                        {
+                            val message = task.exception!!.toString()
+                            Toast.makeText(this, "Error: $message" , Toast.LENGTH_LONG).show()
+                            mAuth.signOut()
+                            progressDialog.dismiss()
+                        }
+                    }
+            }
+        }
+    }
+
+    private fun saveUserInfo(fullName: String, userName: String, email: String, password: String, progressDialog: ProgressDialog)
+    {
+        val currentUserID = FirebaseAuth.getInstance().currentUser!!.uid
+        val usersRef: DatabaseReference = FirebaseDatabase.getInstance().reference.child("Users")
+        val userMap = HashMap<String, Any>()
+
+        userMap["uid"] = currentUserID
+        userMap["fullname"] = fullName
+        userMap["username"] = userName
+        userMap["email"] = email
+        userMap["bio"] = "Welcome to my Instagram Clone App!"
+        userMap["image"] = "https://firebasestorage.googleapis.com/v0/b/instagram-clone-app-7f61a.appspot.com/o/Default%20Images%2Fprofile.png?alt=media&token=1d051bfb-77fd-4404-9652-95836cfb796f"
+
+        usersRef.child(currentUserID).setValue(userMap)
+            .addOnCompleteListener {task ->
+                if (task.isSuccessful)
+                {
+                    progressDialog.dismiss()
+                    Toast.makeText(this, "Account has been created successfully.", Toast.LENGTH_LONG).show()
+
+                    val intent = Intent(this, MainActivity::class.java)
+                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
+                    startActivity(intent)
+                    finish()
+                }
+                else
+                {
+                    val message = task.exception!!.toString()
+                    Toast.makeText(this, "Error: $message" , Toast.LENGTH_LONG).show()
+                    FirebaseAuth.getInstance().signOut()
+                    progressDialog.dismiss()
+                }
+            }
     }
 }
